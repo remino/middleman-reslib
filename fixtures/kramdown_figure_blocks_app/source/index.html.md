d9ef77f19ab0d03b1925218deafee03aa4f9b926
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-22 20:58:27 +0900

    Allow multiple images in figure block with caption

--- fixtures/kramdown_figure_blocks_app/source/index.html.md
+++ fixtures/kramdown_figure_blocks_app/source/index.html.md
@@ -1,14 +1,15 @@
 ## Figure with single image and caption
 
-![img1](img.gif)
-Image
+![img1](img.gif) Image
 
-## Figure with multiple images
+## Figure with multiple images and caption
 
-![img2](img.gif)
-![img3](img.gif)
+![img2](img.gif) ![img3](img.gif) Image
+
+## Figure with multiple images and no caption
+
+![img2](img.gif) ![img3](img.gif)
 
 ## Figure with linked images
 
-[![img4](img.gif)](#)
-[![img5](img.gif)](#)
+[![img4](img.gif)](#) [![img5](img.gif)](#)
--- lib/middleman-reslib/kramdown_figure_blocks/override.rb
+++ lib/middleman-reslib/kramdown_figure_blocks/override.rb
@@ -1,25 +1,31 @@
-require 'kramdown'
+require "kramdown"
 
 class ::Kramdown::Converter::Html
-	def starts_with_image?(el)
-		el.children.first.type == :img ||
-			(el.children.first.type == :a && el.children.first.children.first.type == :img)
-	end
+  def starts_with_image?(el)
+    el.children.first.type == :img ||
+      (el.children.first.type == :a && el.children.first.children.first.type == :img)
+  end
 
-	def convert_p(el, indent)
-		if el.options[:transparent]
-			inner(el, indent)
-		else
-			tag = starts_with_image?(el) ? 'figure' : 'p'
+  def convert_p(el, indent)
+    if el.options[:transparent]
+      inner(el, indent)
+    else
+      tag = starts_with_image?(el) ? "figure" : "p"
 
-			if tag == 'figure'
-				html_image = el.children.first
-				html_figcaption = el.children.slice(1..-1).map { |c| convert c }.join
-				inner_html = "#{convert(html_image)}<figcaption>#{html_figcaption}</figcaption>"
-				format_as_block_html(tag, el.attr, inner_html, indent)
-			else
-				format_as_block_html(tag, el.attr, inner(el, indent), indent)
-			end
-		end
-	end
+      if tag == "figure"
+        html_images = []
+
+        while el.children.first.type == :img || (el.children.first.type == :a && el.children.first.children.first.type == :img)
+          html_images << convert(el.children.first)
+          el.children.shift
+        end
+
+        html_figcaption = el.children.map { |c| convert c }.join
+        inner_html = "#{html_images.join("")}<figcaption>#{html_figcaption}</figcaption>"
+        format_as_block_html(tag, el.attr, inner_html, indent)
+      else
+        format_as_block_html(tag, el.attr, inner(el, indent), indent)
+      end
+    end
+  end
 end
